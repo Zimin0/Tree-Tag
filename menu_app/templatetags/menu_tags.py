37849d46f75e0bdc34db4790d3df250e6e89f940
Zimin0 <you@example.com>
2023-10-11 19:13:32 +0300

    template tag rewrited

--- menu_app/templatetags/menu_tags.py
+++ menu_app/templatetags/menu_tags.py
@@ -6,5 +6,16 @@ register = template.Library()
 @register.inclusion_tag('menu_app/menu.html', takes_context=True)
 def draw_menu(context, menu_name):
     request = context['request']
-    menu_items = MenuItem.objects.filter(menu_name=menu_name)
-    return {'menu_items': menu_items, 'request': request}
+    active_item = MenuItem.objects.filter(menu_name=menu_name, url=request.path).first()
+    print(active_item, menu_name, request.path)
+    if active_item:
+        parents = list(active_item.get_ancestors()) + [active_item,]
+        parent_ids = [parent.id for parent in parents] + [active_item.id,] 
+        children = list(active_item.get_children()) 
+    else:
+        base = MenuItem.objects.filter(menu_name=menu_name, url='/').first()
+        parents = [base, ]
+        parent_ids = [base.pk, ]
+        children = []
+    return {'active_item': active_item, 'parents': parents, 'parent_ids': parent_ids, 'children': children, 'request': request}
+
